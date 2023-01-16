7717d7328c3e6465ea8692f0add3002deb83c29d
Marcus Romano <marcusvromano02@gmail.com>
2023-01-16 20:50:01 -0300

    Initial Commit (Backend Structure)

---
+++ server/src/server.ts
@@ -0,0 +1,21 @@
+import Fastify from "fastify";
+import cors from '@fastify/cors'
+import { PrismaClient } from "@prisma/client";
+
+const app = Fastify()
+const prisma = new PrismaClient()
+
+app.register(cors);
+
+app.get("/", async (req, res) => { 
+    const habits = await prisma.habit.findMany()
+
+
+    return habits
+});
+
+app.listen({ 
+    port: 3333, 
+}).then(() => {
+    console.log("Server listening on port 3333");
+})
